0a1ef33e6533f276e2dc911cea7cd11cbe1cc7dd
Nicolas Loew <ympker@web.de>
2020-12-08 21:46:26 +0100

    Create boilerplateplugin.php

---
+++ plugintemplate/boilerplateplugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Site Plugin for example.com
+Description: Site specific code changes for example.com
+*/
+/* Start Adding Functions Below this Line */
+  
+  
+/* Stop Adding Functions Below this Line */
+?>
