0fbadd7c5497e9597120f70962c13b8750967271
Nicolas Loew <ympker@web.de>
2022-02-18 22:43:01 +0100

    Update fa-v600-local.php

--- local-fontawesome/fa-v600-local/fa-v600-local.php
+++ local-fontawesome/fa-v600-local/fa-v600-local.php
@@ -6,7 +6,7 @@ Description: This Plugin enables you to host Fontawesome v6.0.0 locally to bette
 /* Start Adding Functions Below this Line */
  
  /* Register and add local Fontawesome Stylesheet v6.0.0 Web
- For reference, read: https://developer.wordpress.org/reference/functions/wp_enqueue_script/ 
+ For reference, read: https://developer.wordpress.org/reference/functions/wp_enqueue_script/ and https://developer.wordpress.org/reference/hooks/wp_enqueue_scripts/
  and https://developer.wordpress.org/reference/functions/wp_register_style/#description
  */
 function add_fonts() {
